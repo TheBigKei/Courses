119b3262f32fdc588f685bf0408ea58bb7542288
TheBigKei <TheBigKei@yandex.ru>
2023-12-16 16:00:42 +0300

    md1_hw1

--- MainProjectHW RU/pages/Module 2/My_name/scripts/script.js
+++ MainProjectHW RU/pages/Module 2/My_name/scripts/script.js
@@ -1,2 +1,8 @@
-let message = 'Alexander';
-alert = (message);
+document.addEventListener('DOMContentLoaded', function() {
+    let message = 'Say my name...';
+    let message1 = 'Heisenberg';
+    let message2 = "You're goddamn right";
+    alert(message);
+    alert(message1);
+    alert(message2);
+});
