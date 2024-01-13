ba5ad2677e05be7143d1a9e9fb3e5aef3cced6ba
TheBigKei <TheBigKei@yandex.ru>
2024-01-13 10:38:14 +0300

    1

---
+++ MainProjectHW RU/pages/Module 2/Class_employee/scripts/script.js
@@ -0,0 +1,19 @@
+//Task 1
+class Employee{
+    name = 'Oleg';
+    age = '22';
+    salary = '95000';
+}
+
+console.log("Start of the task 1");
+
+let oleg = new Employee;
+console.log(oleg);
+
+console.log("End of the task 1\n ");
+
+console.log("Start of the task 1");
+
+console.log('Name = ' + oleg.name + '\nAge = ' + oleg.age + '\nSalary = ' + oleg.salary);
+
+console.log("End of the task 1\n ");
